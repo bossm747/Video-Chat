e19f86173d7bba9db9b0fc58c48336f39c36b2fc
bossm747 <142097219+bossm747@users.noreply.github.com>
2024-10-25 22:39:20 +0400

    Update project configuration and metadata for Eunica Band Messenger
    * **Firebase configuration**
      - Change the default project to "eunica-band-messenger" in `.firebaserc`
    
    * **HTML metadata**
      - Change the title to "Eunica Band Messenger"
      - Change the description to "Eunica Band Messenger built using React, Web RTC and Socket.io"
      - Change the canonical link to "https://eunica-band-messenger.web.app/" in `index.html`
    
    * **Manifest metadata**
      - Change the short_name to "Eunica Band"
      - Change the name to "Eunica Band Messenger" in `manifest.json`
    
    * **Footer component**
      - Change the footer link to "https://github.com/mihir0699/Eunica-Band-Messenger"
      - Change the footer text to "Made with <HeartTwoTone twoToneColor='#eb2f96' /> By Eunica Band Team" in `Footer.js`

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -8,12 +8,12 @@ const Footer = () => {
       Made with <HeartTwoTone twoToneColor="#eb2f96" /> By{" "}
       <a
         className="footer__link"
-        href="https://github.com/mihir0699/Video-Chat"
+        href="https://github.com/mihir0699/Eunica-Band-Messenger"
         target="_blank"
         style={{ color: "#fdfdfd" }}
         rel="noreferrer"
       >
-        Mihir Gupta
+        Eunica Band Team
       </a>
     </footer>
   );
